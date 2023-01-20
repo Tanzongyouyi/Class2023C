0e0edf83ef697991f14a52887150075aae505f55
TanzongYouyi <2701262643@qq.com>
2023-01-20 20:12:36 +0800

    getchar使用方法

--- 课程/P32 循环语句while 1.c
+++ 课程/P32 循环语句while 1.c
@@ -5,15 +5,36 @@
 //continue的用法 用于跳过本次循环continue后面的代码,直接去判断部分,看是否进行下一次循环
 
 #include <stdio.h>
-int main()
-{
-	int i = 1;
-	while (i <= 10)
-	{
-		if (i == 5)
-			continue;
-		printf("%d ", i);
-		i++;
-	}
-	return 0;
-}
+//int main()
+//{
+//	int i = 1;
+//	while (i <= 10)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i);
+//		i++;
+//	}
+//	return 0;
+//}
+
+//getchar的使用
+//EOF - end of file - 文件结束标志
+//int main()
+//{
+//	int ch = getchar();
+//	//printf("%c\n", ch);//或者下一行所示的代码
+//	putchar(ch);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+//ctrl + z - getchar就读取结束
