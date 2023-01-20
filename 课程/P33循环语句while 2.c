a609c9745baef68e3439f69fccce0e5d396c659b
TanzongYouyi <2701262643@qq.com>
2023-01-20 21:16:54 +0800

    while 2结束

---
+++ 课程/P33循环语句while 2.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+//int main()
+//{
+//	char password[20] = { 0 };
+//	printf("请输入密码:>");
+//	scanf("%s", password);
+//	printf("请确认密码(Y/N):>");
+//	//只要再写一个getchar就能清理缓冲区
+//	getchar();//把\n处理掉了
+//	int tmp = 0;
+//	while ((tmp = getchar()) != '\n')
+//	{
+//		;
+//	}
+//	int ch = getchar();
+//	if (ch == 'Y')
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("确认失败\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')
+//			continue;
+//
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+
+
+
