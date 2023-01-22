e85201b752bfb75633caf57b50347d21ebc06209
TanzongYouyi <2701262643@qq.com>
2023-01-22 19:36:11 +0800

    P35!

---
+++ 课程/P34循环语句for.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+//for循环语法
+//int main()
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//		if (i == 5)
+//			continue;
+//		printf("%d ", i); //即 初始化;判断;调整
+//	}
+//}
+
+//前闭后开
+//int main()
+//{
+//	int arr[10] = { 0 };
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+//省略  不要随便省略!!!
+//判断部分如果省略就是死循环
+//int main()
+//{
+//	for (; ;)
+//	{
+//		printf("exe\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (; i < 3; i++)
+//	{
+//		for (; j < 3; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+// =是赋值!!! ==是判断!!!
