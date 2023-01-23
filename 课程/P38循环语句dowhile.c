1641edda029595ae27c41a0639303e735ce3b51c
TanzongYouyi <2701262643@qq.com>
2023-01-23 21:10:45 +0800

    P38

---
+++ 课程/P38循环语句dowhile.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+//do while”Ô∑®
+//int main()
+//{
+//	int i = 1;
+//	do
+//	{
+//		printf("%d ", i);
+//		i++;
+//	} while (i <= 10);
+//	return 0;
+//}
+
+//¡∑œ∞“ª
+//int main()
+//{
+//	int i = 0;
+//	int n = 0;
+//	int m = 1;
+//	for (i = 1; i <= n; i++)
+//	{
+//		m *= i;
+//	}
+//	printf("%d\n", m);
+//	return 0;
+//}
+
+//¡∑œ∞∂˛
+//int main()
+//{
+//	int n = 0;
+//	int num = 0;
+//	int i = 1, b = 1;
+//	scanf_s("%d", &n);
+//	for (i = 1; i <= n; i++)
+//	{
+//		b *= i;
+//		num += b;
+//	}
+//	printf("%d\n", num);
+//	return 0;
+//}
+
+//¡∑œ∞»˝
+int main()
+{
+
+	return 0;
+}
