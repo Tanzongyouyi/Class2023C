5fb322e5f23518242173cba7ca8ec2188d969449
TanzongYouyi <2701262643@qq.com>
2023-05-28 16:54:02 +0800

    #emmm

---
+++ 课程/P43休息答疑.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//int main()
+//{
+//flag:
+//	printf("hehe\n");
+//	printf("haha\n");
+//
+//	goto flag;
+//	return 0;
+//}
+
+//shutdown -s -t 60
+//shutdown -a
+int main()
+{
+	//ÓÐ¸öº¯Êý½Ðsystem
+	
+	return 0;
+}
